6d6e8a1a8cd67611c1c2e5c874a2975a0156ac7f
Ari Lathifatul Chusna <ari.lathifatul.chusna-2020@vokasi.unair.ac.id>
2023-02-04 12:16:30 +0700

    layouts

--- app/Views/main/layouts.php
+++ app/Views/main/layouts.php
@@ -187,15 +187,6 @@
       <div class="card">
         <div class="card-header">
           <h3 class="card-title">  <?= $this->renderSection('subjudul')?></h3>
-
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
         </div>
         <div class="card-body">
         <?= $this->renderSection('isi')?>
