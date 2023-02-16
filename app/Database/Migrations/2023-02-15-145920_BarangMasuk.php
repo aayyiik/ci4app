d718f311bb777ca2542880c3a226c4ff131a0ced
Ari Lathifatul Chusna <ari.lathifatul.chusna-2020@vokasi.unair.ac.id>
2023-02-16 14:40:57 +0700

    migrasi brgmsk

---
+++ app/Database/Migrations/2023-02-15-145920_BarangMasuk.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class BarangMasuk extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'idfaktur' => [
+                'type' => 'char',
+                'constraint' => '5',
+            ],
+            'tglfaktur' => [
+                'type' => 'date',
+            ],
+            'totalharga' => [
+                'type' => 'double',
+            ]
+            ]);
+
+            $this->forge->addPrimaryKey('idfaktur');
+            $this->forge->createTable('barangmasuk');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('barangmasuk');
+    }
+}
---
+++ app/Database/Migrations/2023-02-16-145959_TempBarangMasuk.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TempBarangMasuk extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'iddetail' => [
+                'type' => 'char',
+                'constraint' => '5',
+            ],
+            'detidfaktur' => [
+                'type' => 'char',
+                'constraint' => '5'
+            ],
+            'detbrgkode' => [
+                'type' => 'char',
+                'constraint' => '5'
+            ],
+            'dethargamasuk' => [
+                'type' => 'double',
+            ],
+            'dethargajual' => [
+                'type' => 'double',
+            ],
+            'detjumlah' => [
+                'type' => 'int',
+            ],
+            'detsubtotal' => [
+                'type' => 'double'
+            ]
+            ]);
+
+            $this->forge->addPrimaryKey('iddetail');
+            $this->forge->addForeignKey('detidfaktur', 'barangmasuk', 'idfaktur');
+            $this->forge->addForeignKey('detbrgkode', 'barang','brgkode');
+
+            $this->forge->createTable('tempbarangmasuk');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tempbarangmasuk');
+    }
+}
