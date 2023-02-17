d57ce748cee39e1e3c2f489a6590cc51111fb4ac
Ari Lathifatul Chusna <ari.lathifatul.chusna-2020@vokasi.unair.ac.id>
2023-02-17 12:00:18 +0700

    form tambah brgmsk

--- app/Views/barangmasuk/formbarangmasuk.php
+++ app/Views/barangmasuk/formbarangmasuk.php
@@ -1,12 +1,72 @@
-<?= $this->extend('main/layouts') ;?>
-<?= $this->section('judul') ;?>
+<?= $this->extend('main/layouts'); ?>
+<?= $this->section('judul'); ?>
 Barang Masuk
-<?= $this->endSection('judul') ;?>
-<?= $this->section('subjudul') ;?>
-Form Barang Masuk
-<?= $this->endSection('subjudul') ;?>
-<?= $this->section('isi') ;?>
-<!-- CODE HERE -->
-<?= $this->endSection('isi') ;?>
+<?= $this->endSection('judul'); ?>
+<?= $this->section('subjudul'); ?>
 
+<?= $this->endSection('subjudul'); ?>
+<?= $this->section('isi'); ?>
 
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="inputCity" class="form-label">No. Faktur Barang Masuk</label>
+        <input type="text" class="form-control" id="detidfaktur" name="detidfaktur">
+    </div>
+    <div class="form-group col-md-6">
+        <label for="inputCity" class="form-label">Tgl. Barang Masuk</label>
+        <input type="date" class="form-control" id="tglbrgmasuk" name="dettglfaktur" value="<?= ('Y-m-d') ?>">
+    </div>
+</div>
+
+<div class="card">
+    <div class="card-header">
+        Card Data Barang
+    </div>
+    <div class="card-body">
+        <div class="form-row">
+            <div class="form-group col-md-3">
+                <label for="kodebarang" class="form-label">Kode Barang</label>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Kode Barang">
+                    <button class="btn btn-outline-primary" type="button" id="btncaribarang">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </div>
+
+            <div class="form-group col-md-3">
+                <label for="namabarang" class="form-label">Nama Barang</label>
+                <input type="text" class="form-control" id="namabarang" name="namabarang" readonly>
+            </div>
+
+            <div class="form-group col-md-2">
+                <label for="namabarang" class="form-label">Harga Jual</label>
+                <input type="text" class="form-control" id="namabarang" name="namabarang" readonly>
+            </div>
+
+            <div class="form-group col-md-2">
+                <label for="namabarang" class="form-label">Harga Beli</label>
+                <input type="text" class="form-control" id="namabarang" name="namabarang">
+            </div>
+
+            <div class="form-group col-md-1">
+                <label for="namabarang" class="form-label">Jumlah</label>
+                <input type="text" class="form-control" id="namabarang" name="namabarang">
+            </div>
+
+            <div class="form-group col-md-1">
+                <label for="namabarang" class="form-label">Aksi</label><br>
+                <button class="btn btn-sm btn-info" type="button" id="add">
+                    <i class="fa fa-plus-square"></i>
+                </button>
+                <button class="btn btn-sm btn-warning" type="button" id="reload">
+                    <i class="fa fa-sync-alt"></i>
+                </button>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+<?= $this->endSection('isi'); ?>
--- app/Views/main/layouts.php
+++ app/Views/main/layouts.php
@@ -5,6 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>AdminLTE 3 | Blank Page</title>
 
+  <!-- jQuery -->
+  <script src="<?= base_url() ?>/plugins/jquery/jquery.min.js"></script>
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
@@ -225,8 +227,7 @@
 </div>
 <!-- ./wrapper -->
 
-<!-- jQuery -->
-<script src="<?= base_url() ?>/plugins/jquery/jquery.min.js"></script>
+
 <!-- Bootstrap 4 -->
 <script src="<?= base_url() ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
